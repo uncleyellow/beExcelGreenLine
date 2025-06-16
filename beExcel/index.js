2336e5ee402ab4561948d35025e583b7b9fc1926
uncleyellowdk@ratraco.vn <uncleyellowdk@ratraco.vn>
2025-06-16 15:33:16 +0700

    thêm hàng lẻ api

--- beExcel/index.js
+++ beExcel/index.js
@@ -1007,7 +1007,7 @@ app.get('/api/totals', async (req, res) => {
 // FLC APIs
 app.get('/api/flc', async (req, res) => {
     try {
-        const rows = await handleSheetOperation('get', 'FLC!A4:E');
+        const rows = await handleSheetOperation('get', 'FLC!A2:G');
         if (!rows || rows.length === 0) {
             return res.status(404).json({ message: 'Không có dữ liệu' });
         }
